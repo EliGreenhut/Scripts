4763dc3cdeeb218ec07e02eebf5c2988c75b16fe
EliGreenhut <elig@securitydam.com>
2017-11-01 13:19:36 +0200

    Initial version

---
+++ auto_upgrade.py
@@ -0,0 +1,44 @@
+from fabric.api import env, run
+from fabric.context_managers import settings
+
+env.hosts = ['10.20.4.228']  # Default remote host IP address. Overidden during deployment
+env.user = 'root'  # Default VMs user name
+env.password = 'securitydam'
+
+NAVIGATE = 'cd /root/automation_japan/'
+
+##Master Setup (Test setup)
+UPGRADE_1 = 'sdcc_upgrade_tni:japan-sp1,10.20.4.81,10.20.4.4,10.20.4.70,all'
+UPGRADE_11 = 'sdcc_upgrade_mbi:japan-sp1,10.20.4.132,10.20.4.4,all'
+
+##Screen comparison
+UPGRADE_2 = 'sdcc_upgrade_sni:japan-sp1,10.20.4.72,all'
+
+## MSA Setup
+UPGRADE_3 = 'sdcc_upgrade_sni:japan-sp1,10.20.4.143,all'
+
+##Master Setup (Previously Ireland setup)
+UPGRADE_4 = 'sdcc_upgrade_tni:japan,10.20.4.68,10.20.4.122,10.20.4.8,all'
+UPGRADE_41 = 'sdcc_upgrade_mbi:japan,10.20.4.77,10.20.4.122,all'
+
+##MSSP Setup
+UPGRADE_5 = 'sdcc_upgrade_sni:mssp_2.2,10.20.4.178,all'
+
+UPGRADES = [UPGRADE_1,
+            UPGRADE_11,
+            UPGRADE_2,
+            UPGRADE_3,
+            UPGRADE_4,
+            UPGRADE_41,
+            UPGRADE_5]
+
+UPGRADE_STRING = '"' + '","'.join(upgrade.replace(',', '\,') for upgrade in UPGRADES) + '"'
+
+
+def run_update():
+    with settings(host_string=env.hosts[0]):
+        run('fab -f /root/automation_japan/fabfile.py auto_upgrade:' + UPGRADE_STRING)
+
+
+if __name__ == '__main__':
+    run_update()
