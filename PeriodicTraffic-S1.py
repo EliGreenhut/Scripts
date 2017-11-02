adb1970c46ecdec80aa1ec00169169690760da75
EliGreenhut <elig@securitydam.com>
2017-11-02 18:42:18 +0200

    Initial Version

--- PeriodicTraffic-S1.py
+++ PeriodicTraffic-S1.py
@@ -2,7 +2,7 @@ from fabric.api import env, run
 from fabric.context_managers import settings
 
 env.hosts = ['10.20.4.238']  # Default remote host IP address. Overidden during deployment
-env.user = 'root'            # Default VMs user name
+env.user = 'root'  # Default VMs user name
 env.password = 'radware'
 
 def run_update():
@@ -13,4 +13,4 @@ def run_update():
         run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 5 --device 10.20.6.10 --policy PO-ST-S1-1')
 
 if __name__ == '__main__':
-        run_update()
+    run_update()
