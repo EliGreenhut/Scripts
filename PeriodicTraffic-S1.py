fe33fa6df90bd64b032c05516179a2140937fd6c
EliGreenhut <elig@securitydam.com>
2017-11-05 16:17:35 +0200

    Initial Version Update mssp version to be 2.3 should be use folder automation_mssp_2.3

--- PeriodicTraffic-S1.py
+++ PeriodicTraffic-S1.py
@@ -1,11 +1,12 @@
 from fabric.api import env, run
 from fabric.context_managers import settings
+from fabric.decorators import parallel
 
 env.hosts = ['10.20.4.238']  # Default remote host IP address. Overidden during deployment
 env.user = 'root'  # Default VMs user name
 env.password = 'radware'
 
-def run_update():
+def run_command():
     with settings(host_string=env.hosts[0]):
         run('python DP_TrafficUtilizationSimulator.py --rate 50 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
         run('python DP_TrafficUtilizationSimulator.py --rate 70 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
@@ -13,4 +14,4 @@ def run_update():
         run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
 
 if __name__ == '__main__':
-    run_update()
+    run_command()
--- PeriodicTraffic-S2.py
+++ PeriodicTraffic-S2.py
@@ -1,10 +1,16 @@
 from fabric.api import env, run
 from fabric.context_managers import settings
+from fabric.decorators import parallel
 
-ssh = SSHExtensions('10.20.4.238', 'root', 'radware')
-ssh.Send('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 10 --device 10.20.6.20 --policy PO-S2-N-Traf1')
+#env.hosts = ['10.20.4.238']  # Default remote host IP address. Overidden during deployment
+env.user = 'root'  # Default VMs user name
+env.password = 'radware'
 
-ssh = SSHExtensions('10.20.4.238', 'root', 'radware')
-ssh.Send('python DP_TrafficUtilizationSimulator.py --rate 65 --duration 10 --device 10.20.6.20 --policy PO-S2-N-Traf2')
+@parallel
+def run_command(cmd, host):
+    with settings(host_string=host):
+        run(cmd)
 
-
+if __name__ == '__main__':
+    run_command('python DP_TrafficUtilizationSimulator.py --rate 50 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1', '10.20.4.237')
+    run_command('python DP_TrafficUtilizationSimulator.py --rate 50 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1', '10.20.4.238')
