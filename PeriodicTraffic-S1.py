2505fa56a8937fac655e1515d2cd101cb8ce2123
EliGreenhut <elig@securitydam.com>
2017-11-05 16:18:50 +0200

    Initial Version Update mssp version to be 2.3 should be use folder automation_mssp_2.3

--- PeriodicTraffic-S1.py
+++ PeriodicTraffic-S1.py
@@ -9,9 +9,9 @@ env.password = 'radware'
 def run_command():
     with settings(host_string=env.hosts[0]):
         run('python DP_TrafficUtilizationSimulator.py --rate 50 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
-        run('python DP_TrafficUtilizationSimulator.py --rate 70 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
+        run('python DP_TrafficUtilizationSimulator.py --rate 70 --duration 15 --device 10.20.6.10 --policy PO-ST-S1-1')
         run('python DP_TrafficUtilizationSimulator.py --rate 90 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
-        run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
+        run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 15 --device 10.20.6.10 --policy PO-ST-S1-1')
 
 if __name__ == '__main__':
     run_command()
