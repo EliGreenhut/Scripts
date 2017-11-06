78c8d1a1a27c4e0da5724acb225c0ae3b7111fa9
EliGreenhut <elig@securitydam.com>
2017-11-06 10:45:47 +0200

    Make it run in parallel

--- PeriodicTraffic-S2.py
+++ PeriodicTraffic-S2.py
@@ -7,8 +7,9 @@ env.password = 'radware'
 
 def run_command(cmd, host):
     with settings(host_string=host):
-        run(cmd)
+        r = run(cmd, pty=False)
+        print ''
 
 if __name__ == '__main__':
-    run_command('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-S2-N-Traf1', '10.20.4.237')
-    run_command('python DP_TrafficUtilizationSimulator.py --rate 65 --duration 15 --device 10.20.6.20 --policy PO-S2-N-Traf2', '10.20.4.237')
+    run_command('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-S2-N-Traf1 >& /dev/null < /dev/null &', '10.20.4.237')
+    run_command('python DP_TrafficUtilizationSimulator.py --rate 65 --duration 15 --device 10.20.6.20 --policy PO-S2-N-Traf2 >& /dev/null < /dev/null &', '10.20.4.237')
