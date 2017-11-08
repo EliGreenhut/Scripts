5cffb2c134a56090bd1cda03464227e184f172d7
EliGreenhut <elig@securitydam.com>
2017-11-08 13:40:06 +0200

    Removing upgrade of 10.20.4.143 (temporary setup for MSA)

---
+++ PeriodicTraffic-M1.py
@@ -0,0 +1,17 @@
+from fabric.api import env, run
+from fabric.context_managers import settings
+from fabric.decorators import parallel
+
+env.hosts = ['10.20.4.238']  # Default remote host IP address. Overidden during deployment
+env.user = 'root'  # Default VMs user name
+env.password = 'radware'
+
+def run_command():
+    with settings(host_string=env.hosts[0]):
+        run('python DP_TrafficUtilizationSimulator.py --rate 25 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1')
+        run('python DP_TrafficUtilizationSimulator.py --rate 45 --duration 15 --device 10.20.6.10 --policy PO-M1-Traffic1')
+        run('python DP_TrafficUtilizationSimulator.py --rate 65 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1')
+        run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 15 --device 10.20.6.10 --policy PO-M1-Traffic1')
+
+if __name__ == '__main__':
+    run_command()
