7ae5a440b2ca78e53b05da383e84b657d2970ea9
EliGreenhut <elig@securitydam.com>
2017-11-16 14:59:18 +0200

    Initial version of the full processes control

--- BackgroundProcess.py
+++ BackgroundProcess.py
@@ -3,92 +3,69 @@ from fabric.context_managers import settings
 # from fabric.decorators import parallel
 import time
 
-env.user = 'root'  # Default VMs user name
-env.password = 'radware'
+
 
 # -----------------------------------------------------
 #  Traffic directly on site
 # -----------------------------------------------------
-def run_command():
+def traffic_site():
     with settings(host_string='10.20.4.238'):
-        run('python DP_TrafficUtilizationSimulator.py --rate 25 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
-        run('python DP_TrafficUtilizationSimulator.py --rate 45 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
-        run('python DP_TrafficUtilizationSimulator.py --rate 65 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
-        run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1')
-
-if __name__ == '__main__':
-    run_command()
-
-time.sleep (900)
+        run('python DP_TrafficUtilizationSimulator.py --rate 25 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 45 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 65 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 10 --device 10.20.6.10 --policy PO-ST-S1-1', pty=False)
 
 # -----------------------------------------------------------------
 # Traffic in two assets (non-MSA), in parallel, related to one site
 # -----------------------------------------------------------------
-def run_command(cmd):
+def traffic_2assets(cmd):
     with settings(host_string='10.20.4.237'):
-        run(cmd, pty=False)
-
-if __name__ == '__main__':
-    run_command(
-        'python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-S2-N-Traf1 >& /dev/null < /dev/null &')
-    run_command(
-        'python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-S2-N-Traf2 >& /dev/null < /dev/null &')
-
-time.sleep (900)
+        run('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-S2-N-Traf1 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-S2-N-Traf2 >& /dev/null < /dev/null &', pty=False)
 
 # -----------------------------------------------------
 # Traffic in one asset (MSA) related to three sites
 # -----------------------------------------------------
-def run_command():
+def traffic_msa_asset():
     with settings(host_string='10.20.4.238'):
-        run(
-            'python DP_TrafficUtilizationSimulator.py --rate 25 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1')
-        run(
-            'python DP_TrafficUtilizationSimulator.py --rate 45 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1')
-        run(
-            'python DP_TrafficUtilizationSimulator.py --rate 65 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1')
-        run(
-            'python DP_TrafficUtilizationSimulator.py --rate 30 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1')
-
-if __name__ == '__main__':
-    run_command()
-
-time.sleep (1200)
+        run('python DP_TrafficUtilizationSimulator.py --rate 25 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 45 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 65 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 10 --device 10.20.6.10 --policy PO-M1-Traffic1', pty=False)
 
 # -----------------------------------------------------
 # Attack on Network asset
 # -----------------------------------------------------
-def run_command():
+def attack_nasset():
     with settings(host_string='10.20.4.237'):
-        run(
-            'python DP_SecurityAttacksSimulator.py --rate 20 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 70 --duration 10')
-        run(
-            'python DP_SecurityAttacksSimulator.py --rate 40 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 71 --duration 5')
-        run(
-            'python DP_SecurityAttacksSimulator.py --rate 80 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 72 --duration 10')
-        run(
-            'python DP_SecurityAttacksSimulator.py --rate 60 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 73 --duration 5')
-
-
-if __name__ == '__main__':
-    run_command()
-
-time.sleep(900)
+        run('python DP_SecurityAttacksSimulator.py --rate 20 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 70 --duration 10', pty=False)
+        run('python DP_SecurityAttacksSimulator.py --rate 40 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 71 --duration 5', pty=False)
+        run('python DP_SecurityAttacksSimulator.py --rate 80 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 72 --duration 10', pty=False)
+        run('python DP_SecurityAttacksSimulator.py --rate 60 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 73 --duration 5', pty=False)
 
 # -----------------------------------------------------
 # Traffic on Diverted asset (through SC)
 # -----------------------------------------------------
-env.user = 'root'  # Default user name for automation station
-env.password = 'securitydam'
-
-def run_command():
+def ingress_egress():
     with settings(host_string='10.20.4.228'):
-        run(
-            'python /root/automation_japan/Simulators/NetflowSimulator.py --rate 30 --duration 10 --asset 111.1.6.0 --sdcc 10.20.4.132 --port 9996')
-        run(
-            'python /root/automation_japan/Simulators/NetflowSimulator.py --rate 20 --duration 10 --asset 111.1.6.0 --sdcc 10.20.4.132 --port 9995')
-
-time.sleep(1500)
+        run('python /root/automation_japan/Simulators/NetflowSimulator.py --rate 30 --duration 10 --asset 111.1.6.0 --sdcc 10.20.4.132 --port 9996', pty=False)
+        run('python /root/automation_japan/Simulators/NetflowSimulator.py --rate 20 --duration 10 --asset 111.1.6.0 --sdcc 10.20.4.132 --port 9995', pty=False)
 
 if __name__ == '__main__':
-    run_command()
+    env.user = 'root'  # Default VMs user name
+    env.password = 'radware'
+
+    traffic_site()
+    time.sleep(900)
+    traffic_2assets()
+    time.sleep(900)
+    traffic_msa_asset()
+    time.sleep(1200)
+    attack_nasset()
+    time.sleep(900)
+
+    env.user = 'root'  # Default user name for automation station
+    env.password = 'securitydam'
+
+    ingress_egress()
+    time.sleep(1500)
