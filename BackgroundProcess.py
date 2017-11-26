a0428cec7af806ee0af1f9f88cb2ec1036448e3c
EliGreenhut <elig@securitydam.com>
2017-11-26 15:29:52 +0200

    Set two attack types on single asset

--- BackgroundProcess.py
+++ BackgroundProcess.py
@@ -41,8 +41,9 @@ def attack_nasset():
     with settings(host_string='10.20.4.237'):
         run('python DP_SecurityAttacksSimulator.py --rate 20 --policy PO-S2-N-Atk1 --device 10.20.6.20 --dest 200.10.14.0 --attack 70 --duration 10', pty=False)
         run('python DP_SecurityAttacksSimulator.py --rate 40 --policy PO-S2-N-Atk2 --device 10.20.6.20 --dest 200.10.15.0 --attack 71 --duration 5', pty=False)
-        run('python DP_SecurityAttacksSimulator.py --rate 80 --policy PO-S2-N-Atk3 --device 10.20.6.20 --dest 200.10.16.0 --attack 72 --duration 10', pty=False)
-        run('python DP_SecurityAttacksSimulator.py --rate 60 --policy PO-S2-N-Atk4 --device 10.20.6.20 --dest 200.10.17.0 --attack 73 --duration 5', pty=False)
+        run('python DP_SecurityAttacksSimulator.py --rate 80 --policy PO-S2-N-Atk3 --device 10.20.6.20 --dest 200.10.16.0 --attack 72 --duration 5', pty=False)
+        run('python DP_SecurityAttacksSimulator.py --rate 80 --policy PO-S2-N-Atk3 --device 10.20.6.20 --dest 200.10.16.0 --attack 73 --duration 5', pty=False)
+        run('python DP_SecurityAttacksSimulator.py --rate 60 --policy PO-S2-N-Atk4 --device 10.20.6.20 --dest 200.10.17.0 --attack 74 --duration 5', pty=False)
 
 
 # -----------------------------------------------------
