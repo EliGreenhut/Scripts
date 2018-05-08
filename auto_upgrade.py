385572f083ebcd3101e6eb61aa19a27f2fde22eb
EliGreenhut <elig@securitydam.com>
2018-05-08 12:07:33 +0300

    Separate upgrade of Kenya fron Japan

--- auto_upgrade.py
+++ auto_upgrade.py
@@ -5,7 +5,8 @@ env.hosts = ['10.20.4.228']  # Default remote host IP address. Overidden during 
 env.user = 'root'  # Default VMs user name
 env.password = 'securitydam'
 
-NAVIGATE = 'cd /root/automation_japan/'
+## NAVIGATE_J = 'cd /root/automation_japan/'
+## NAVIGATE_K = 'cd /root/automation_kenya/'
 
 ##Master Setup (Test setup)
 UPGRADE_1 = 'sdcc_upgrade_tni:master,10.20.4.81,10.20.4.4,10.20.4.70,all'
@@ -21,25 +22,29 @@ UPGRADE_41 = 'sdcc_upgrade_mbi:japan-sp1,10.20.4.77,10.20.4.122,all'
 ##MSSP Setup
 UPGRADE_5 = 'sdcc_upgrade_sni:mssp_2.3,10.20.4.178,all'
 
+UPGRADES_K = [UPGRADE_1,
+              UPGRADE_11]
 
-UPGRADES_J = [UPGRADE_1,
-              UPGRADE_11,
-              UPGRADE_4,
+UPGRADES_J = [UPGRADE_4,
               UPGRADE_41]
 
 UPGRADES_MSSP = [UPGRADE_5]
 
+UPGRADE_STRING_K = '"' + '","'.join(upgrade.replace(',', '\,') for upgrade in UPGRADES_K) + '"'
 UPGRADE_STRING_J = '"' + '","'.join(upgrade.replace(',', '\,') for upgrade in UPGRADES_J) + '"'
 UPGRADE_STRING_MSSP = '"' + '","'.join(upgrade.replace(',', '\,') for upgrade in UPGRADES_MSSP) + '"'
 
 def run_update():
     with settings(host_string=env.hosts[0]):
+        run('fab -f /root/automation_kenya/fabfile.py auto_upgrade:' + UPGRADE_STRING_K)
+        print '########## END OF KENYA SETUP UPGRADE ##########'
+        print '################################################'
         run('fab -f /root/automation_japan/fabfile.py auto_upgrade:' + UPGRADE_STRING_J )
-        print '########## END OF SETUP UPGRADE ##########'
-        print '##########################################'
+        print '########## END OF JAPAN SETUP UPGRADE ##########'
+        print '################################################'
         run('fab -f /root/automation_mssp_2.3/fabfile.py auto_upgrade:' + UPGRADE_STRING_MSSP )
-        print '########## END OF SETUP UPGRADE ##########'
-        print '##########################################'
+        print '########## END OF MSSP SETUP UPGRADE ##########'
+        print '###############################################'
 
 if __name__ == '__main__':
     run_update()
