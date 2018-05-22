f6b7ad716e49aee0252d8a869d472db18cca7ff4
EliGreenhut <elig@securitydam.com>
2018-05-22 09:24:31 +0300

    Adding the "services" argument to overcome failure of execution. The addition is a dummy addition since no need actualy, default is enough

--- auto_upgrade.py
+++ auto_upgrade.py
@@ -9,18 +9,17 @@ env.password = 'securitydam'
 ## NAVIGATE_K = 'cd /root/automation_kenya/'
 
 ##Master Setup (Test setup)
-UPGRADE_1 = 'sdcc_upgrade_tni:master,10.20.4.81,10.20.4.4,10.20.4.70,content=all'
-UPGRADE_11 = 'sdcc_upgrade_mbi:master,10.20.4.132,10.20.4.4,content=all'
+UPGRADE_1 = 'sdcc_upgrade_tni:master,10.20.4.81,10.20.4.4,10.20.4.70,all'
+UPGRADE_11 = 'sdcc_upgrade_mbi:master,10.20.4.132,10.20.4.4,all'
 
 ##Screen comparison
-UPGRADE_2 = 'sdcc_upgrade_sni:master,10.20.4.72,content=all'
+UPGRADE_2 = 'sdcc_upgrade_sni:master,10.20.4.72,all'
 
 ##Master Setup (For migration tests)
-UPGRADE_4 = 'sdcc_upgrade_tni:japan-sp1,10.20.4.68,10.20.4.122,10.20.4.8,content=all'
-UPGRADE_41 = 'sdcc_upgrade_mbi:japan-sp1,10.20.4.77,10.20.4.122,content=all'
-
+UPGRADE_4 = 'sdcc_upgrade_tni:japan-sp1,10.20.4.68,10.20.4.122,10.20.4.8,all'
+UPGRADE_41 = 'sdcc_upgrade_mbi:japan-sp1,10.20.4.77,10.20.4.122,
 ##MSSP Setup
-UPGRADE_5 = 'sdcc_upgrade_sni:mssp_2.3,10.20.4.178,content=all'
+UPGRADE_5 = 'sdcc_upgrade_sni:mssp_2.3,10.20.4.178,all'
 
 UPGRADES_K = [UPGRADE_1,
               UPGRADE_11]
