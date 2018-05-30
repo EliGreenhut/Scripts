cad2d84b8292754edcc66196d8ccb53e11ba4a75
EliGreenhut <elig@securitydam.com>
2018-05-30 10:40:05 +0300

    Change branch from "master" to "kenya"

--- auto_upgrade.py
+++ auto_upgrade.py
@@ -9,11 +9,11 @@ env.password = 'securitydam'
 ## NAVIGATE_K = 'cd /root/automation_kenya/'
 
 ##Master Setup (Test setup)
-UPGRADE_1 = 'sdcc_upgrade_tni:master,10.20.4.81,10.20.4.4,10.20.4.70,all'
-UPGRADE_11 = 'sdcc_upgrade_mbi:master,10.20.4.132,10.20.4.4,all'
+UPGRADE_1 = 'sdcc_upgrade_tni:kenya,10.20.4.81,10.20.4.4,10.20.4.70,all'
+UPGRADE_11 = 'sdcc_upgrade_mbi:kenya,10.20.4.132,10.20.4.4,all'
 
 ##Screen comparison
-UPGRADE_2 = 'sdcc_upgrade_sni:master,10.20.4.72,all'
+UPGRADE_2 = 'sdcc_upgrade_sni:Kenya,10.20.4.72,all'
 
 ##Master Setup (For migration tests)
 UPGRADE_4 = 'sdcc_upgrade_tni:japan-sp1,10.20.4.68,10.20.4.122,10.20.4.8,all'
