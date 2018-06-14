44ade8898cc89eca3a1dc85343471e1ce43e2d3a
EliGreenhut <elig@securitydam.com>
2018-06-14 11:41:06 +0300

    Update branch name in setup 4.70 (kenya changed to kenya_sp1)

--- auto_upgrade.py
+++ auto_upgrade.py
@@ -9,11 +9,11 @@ env.password = 'securitydam'
 ## NAVIGATE_K = 'cd /root/automation_kenya/'
 
 ##Master Setup (Test setup)
-UPGRADE_1 = 'sdcc_upgrade_tni:kenya,10.20.4.81,10.20.4.4,10.20.4.70,all'
-UPGRADE_11 = 'sdcc_upgrade_mbi:kenya,10.20.4.132,10.20.4.4,all'
+UPGRADE_1 = 'sdcc_upgrade_tni:kenya_sp1,10.20.4.81,10.20.4.4,10.20.4.70,all'
+UPGRADE_11 = 'sdcc_upgrade_mbi:kenya_sp1,10.20.4.132,10.20.4.4,all'
 
 ##Screen comparison
-UPGRADE_2 = 'sdcc_upgrade_sni:Kenya,10.20.4.72,all'
+UPGRADE_2 = 'sdcc_upgrade_sni:kenya_sp1,10.20.4.72,all'
 
 ##Master Setup (For migration tests)
 UPGRADE_4 = 'sdcc_upgrade_tni:japan-sp1,10.20.4.68,10.20.4.122,10.20.4.8,all'
@@ -36,13 +36,13 @@ UPGRADE_STRING_MSSP = '"' + '","'.join(upgrade.replace(',', '\,') for upgrade in
 def run_update():
     with settings(host_string=env.hosts[0]):
         run('fab -f /root/automation_kenya/fabfile.py auto_upgrade:' + UPGRADE_STRING_K)
-        print '########## END OF KENYA SETUP UPGRADE ##########'
+        print '########## END OF KENYA_SP1 SETUP UPGRADE ##########'
         print '################################################'
         run('fab -f /root/automation_japan/fabfile.py auto_upgrade:' + UPGRADE_STRING_J )
-        print '########## END OF JAPAN SETUP UPGRADE ##########'
+        print '########## END OF JAPAN-SP1 SETUP UPGRADE ##########'
         print '################################################'
         run('fab -f /root/automation_mssp_2.3/fabfile.py auto_upgrade:' + UPGRADE_STRING_MSSP )
-        print '########## END OF MSSP SETUP UPGRADE ##########'
+        print '########## END OF MSSP_2.3 SETUP UPGRADE ##########'
         print '###############################################'
 
 if __name__ == '__main__':
