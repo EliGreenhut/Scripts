1c2ea6f9c3cc4f0386b1c84fc4ebf012d9084dd0
EliGreenhut <elig@securitydam.com>
2018-06-20 08:37:24 +0300

    Update branch name in setup 4.70 (kenya changed to kenya_sp1), restore setup

--- auto_upgrade.py
+++ auto_upgrade.py
@@ -9,11 +9,8 @@ env.password = 'securitydam'
 ## NAVIGATE_K = 'cd /root/automation_kenya/'
 
 ##Master Setup (Test setup)
-## UPGRADE_1 = 'sdcc_upgrade_tni:kenya_sp1,10.20.4.81,10.20.4.4,10.20.4.70,all'
-## UPGRADE_11 = 'sdcc_upgrade_mbi:kenya_sp1,10.20.4.132,10.20.4.4,all'
-
-UPGRADE_1 = 'sdcc_upgrade_tni:kenya,10.20.4.81,10.20.4.4,10.20.4.70,all'
-UPGRADE_11 = 'sdcc_upgrade_mbi:kenya,10.20.4.132,10.20.4.4,all'
+UPGRADE_1 = 'sdcc_upgrade_tni:kenya_sp1,10.20.4.81,10.20.4.4,10.20.4.70,all'
+UPGRADE_11 = 'sdcc_upgrade_mbi:kenya_sp1,10.20.4.132,10.20.4.4,all'
 
 ##Screen comparison
 UPGRADE_2 = 'sdcc_upgrade_sni:kenya_sp1,10.20.4.72,all'
