d744a7a791212cba78e32080d8e8f30ffdd3a114
EliGreenhut <elig@securitydam.com>
2019-02-14 17:47:16 +0200

    First version

---
+++ AccountTrafficTest.py
@@ -0,0 +1,50 @@
+from fabric.api import env, run
+from fabric.context_managers import settings
+# from fabric.decorators import parallel
+import time
+
+# -----------------------------------------------------
+#  Traffic for 2:30 hours
+# -----------------------------------------------------
+def traffic_all():
+    with settings(host_string='10.20.4.237'):
+# 1.1
+        run('python DP_TrafficUtilizationSimulator.py --rate 25 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS1', pty=False)
+# 1.2
+        run('python DP_TrafficUtilizationSimulator.py --rate 45 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS2', pty=False)
+# 1.3
+        run('python DP_TrafficUtilizationSimulator.py --rate 65 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS3', pty=False)
+# S2
+        run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST2', pty=False)
+# 1.1+1.2+1.3
+        run('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS1 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS2 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS3 >& /dev/null < /dev/null &', pty=False)
+# S1+S3
+        run('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST3 >& /dev/null < /dev/null &', pty=False)
+# 1.1+1.2+1.3+3.1
+        run('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS1 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS2 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS3 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST3-AS1 >& /dev/null < /dev/null &', pty=False)
+# S1
+        run('python DP_TrafficUtilizationSimulator.py --rate 30 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1', pty=False)
+# 1.1+3.1
+        run('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS1 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST3-AS1 >& /dev/null < /dev/null &', pty=False)
+# S1+1.3+S2+3.1
+        run('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST1-AS1 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 40 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST2 >& /dev/null < /dev/null &', pty=False)
+        run('python DP_TrafficUtilizationSimulator.py --rate 75 --duration 15 --device 10.20.6.20 --policy PO-AC1-ST3-AS1 >& /dev/null < /dev/null &', pty=False)
+
+if __name__ == '__main__':
+    env.user = 'root'  # Default VMs user name
+    env.password = 'radware'
+
+    traffic_all()
+    time.sleep(900)
+
+    env.user = 'root'  # Default user name for automation station
+    env.password = 'securitydam'
